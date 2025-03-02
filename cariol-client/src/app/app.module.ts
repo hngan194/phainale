71707374a62fcfffdaa031430e3ec793cb6fa3d6
hngan <ngannh22411c@st.uel.edu.vn>
2025-03-02 21:49:02 +0700

    Converted cariol-client from submodule to normal folder

---
+++ cariol-client/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { BlogComponent } from './components/blog/blog.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutusComponent,
+    BlogComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
